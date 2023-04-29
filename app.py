d444859bd064804347e7bb4734351c575cbcfe74
Bhargav Sarvaria <34936854+bhargav-sarvaria@users.noreply.github.com>
2023-04-29 10:26:41 -0700

    bug fix

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ event = {
 }
 print('Starting watcher...')
 # Continuously poll the S3 bucket for new files
+completed = set()
 while True:
     # Get a list of objects in the S3 bucket
     objects = s3_client.list_objects_v2(Bucket=bucket_name)
@@ -31,19 +32,21 @@ while True:
     # Check if any new objects have been uploaded since the last poll
     if objects['KeyCount'] > 0:
         # Get the name of the latest object
-        latest_object = objects['Contents'][-1]['Key']
-
-        # Update the Lambda function event with the name of the latest object
-        event['Records'][0]['s3']['object']['key'] = latest_object
-
-        print('Triggering lamda function!')
-        # Invoke the Lambda function with the updated event
-        response = lambda_client.invoke(
-            FunctionName=lambda_function_name,
-            InvocationType='Event',
-            Payload=json.dumps(event)
-        )
-        time.sleep(2)
+        # latest_object = objects['Contents'][-1]['Key']
+        latest_objects = objects['Contents']
+        for ob in latest_objects:
+            if ob['Key'] not in completed:
+                completed.add(ob['Key'])
+                # Update the Lambda function event with the name of the latest object
+                event['Records'][0]['s3']['object']['key'] = ob['Key']
+                print(ob['Key'])
+                # Invoke the Lambda function with the updated event
+                response = lambda_client.invoke(
+                    FunctionName=lambda_function_name,
+                    InvocationType='Event',
+                    Payload=json.dumps(event)
+                )
+                time.sleep(1)
     else:
         # Wait for 1 minute before polling again
         time.sleep(10)
